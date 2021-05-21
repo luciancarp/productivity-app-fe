f8c995ed838faa6e18ceea0ff89b7b0256b77049
Lucian Carp <lucian.g.carp@gmail.com>
2021-05-21 14:24:46 +0300

    Add react-toastify

--- src/components/Alerts.tsx
+++ src/components/Alerts.tsx
@@ -1,26 +1,51 @@
+import { useEffect, useState } from 'react'
 import { connect, ConnectedProps } from 'react-redux'
 import { RootStore } from '../store'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { removeAlert } from '../actions/alert'
 
 const mapStateToProps = (state: RootStore) => ({
   alert: state.alert,
 })
-const connector = connect(mapStateToProps, {})
+const connector = connect(mapStateToProps, { removeAlert })
 type PropsFromRedux = ConnectedProps<typeof connector>
 type Props = PropsFromRedux & {}
 
-const Alerts = ({ alert }: Props) => {
-  return (
-    <div>
-      <ul>
-        {alert.map((alert) => (
-          <li>
-            <h3>{alert.variant}</h3>
-            <h2>{alert.message}</h2>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+const Alerts = ({ alert, removeAlert }: Props) => {
+  const [displayed, setDisplayed] = useState<string[]>([])
+
+  useEffect(() => {
+    const storeDisplayed = (id: string) => {
+      setDisplayed([...displayed, id])
+    }
+
+    const removeDisplayed = (id: string) => {
+      setDisplayed([...displayed.filter((key) => id !== key)])
+    }
+
+    alert.forEach((alert) => {
+      if (displayed.includes(alert.id)) return
+
+      storeDisplayed(alert.id)
+
+      toast.error(alert.message, {
+        position: 'bottom-left',
+        autoClose: false,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        onClose: () => {
+          removeDisplayed(alert.id)
+          removeAlert(alert.id)
+        },
+      })
+    })
+  }, [alert, displayed, removeAlert])
+
+  return <ToastContainer />
 }
 
 export default connector(Alerts)
